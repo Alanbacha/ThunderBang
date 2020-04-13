53643f297be318f65c85d1a51b52ccdef84313eb
Alan Bacha <alanbacha@gmail.com>
2020-04-13 15:56:30 -0400

    General adjusts

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -11,6 +11,8 @@ export default function Game() {
 	const [cards, setCards] = useState([]);
 	const [card, setCard] = useState([]);
 
+	const [rules, setRules] = useState([]);
+
 	useEffect(() => {
 		loadCards();
 	}, []);
@@ -29,25 +31,36 @@ export default function Game() {
 		setCard(c);
 	}
 
+	function handleNewRules(description, active) {
+		const r = { description: description, active: active };
+		setRules(rules.push(r));
+	}
+
+	function handleChangeRuleStatus(rule) {
+		rule.active = !rule.active;
+	}
+
 	return (
 		<Layout>
 			<div className="content portrait-flex-direction-column">
 				<section className="playing-card">
-					<label>{card.name}</label>
+					<h2>
+						<label class="card">{card.name}</label>
+					</h2>
 
-					<ul>
-						<li className="language pt-br">
+					<div class="text">
+						<div className="language pt-br">
 							<p>{card.description_PtBr}</p>
-						</li>
+						</div>
 
-						<li className="language en-ca">
+						<div className="language en-ca">
 							<p>{card.description_EnCa}</p>
-						</li>
+						</div>
 
-						<li className="language fr-ca">
+						<div className="language fr-ca">
 							<p>{card.description_FrCa}</p>
-						</li>
-					</ul>
+						</div>
+					</div>
 
 					<button className="button" onClick={handleNewCard}>
 						<span className="language pt-br">Próxima</span>
@@ -56,30 +69,117 @@ export default function Game() {
 					</button>
 				</section>
 
-				<section className="rules-on">
-					<label>
+				<section className="rules">
+					<h2>
 						<span className="language pt-br">Regras ativas</span>
 						<span className="language en-ca">Actives rules</span>
 						<span className="language fr-ca">Règles actives</span>
-					</label>
+					</h2>
 
-					<ul>
-						<li>on</li>
-					</ul>
+					<div className="text">
+						<ul className="rule-list">
+							{rules
+								.filter((r) => r.active === true)
+								.map((r) => (
+									<li>{r}</li>
+								))}
+						</ul>
+					</div>
+
+					<button
+						className="button"
+						onClick={() => {
+							handleNewRules("a", true);
+						}}
+					>
+						<span className="language pt-br">Nova regra</span>
+						<span className="language en-ca">New rule</span>
+						<span className="language fr-ca">Nouvelle Règle</span>
+					</button>
 				</section>
 
-				<section className="rules-off">
-					<label>
+				<section className="rules">
+					<h2>
 						<span className="language pt-br">Regras inativas</span>
 						<span className="language en-ca">Inactives rules</span>
 						<span className="language fr-ca">Règles inactives</span>
-					</label>
+					</h2>
 
-					<ul>
-						<li>off</li>
-					</ul>
+					<div className="text">
+						<ul className="rule-list">
+							{rules
+								.filter((r) => r.active === false)
+								.map((r) => (
+									<li>{r}</li>
+								))}
+						</ul>
+					</div>
+
+					<button
+						className="button"
+						onClick={() => {
+							handleNewRules("b", false);
+						}}
+					>
+						<span className="language pt-br">Nova regra</span>
+						<span className="language en-ca">New rule</span>
+						<span className="language fr-ca">Nouvelle Règle</span>
+					</button>
 				</section>
 			</div>
 		</Layout>
 	);
 }
+
+/*
+ 
+ I just received my grocery shopping, but there are 4 products that didn't came.
+
+Tracking number: 6622016000087
+Total value: $155.96
+
+I want to know if these products will be delivery, or if I will receive the discount of $48.44 in the amount value.
+
+
+__________________________________________________
+PRODUCT 1
+Great Value Beef Burgers 
+Sku: 6000191272333
+items: 1
+price: $15.97
+	
+__________________________________________________
+PRODUCT 2
+Great Value Ham And Cheese Stuffed Breaded Chicken Breast Cutlets
+Sku: 6000196287478
+items: 1
+price $4.50
+
+__________________________________________________
+PRODUCT 3
+Boneless Skinless Fillet Removed Chicken Breasts
+Sku: 6000191279309
+items: 2
+price: $10
+total: $20
+	
+__________________________________________________
+PRODUCT 4
+Lou's Kitchen Barbecue Pork Back Ribs in Lou's Babecue Sauce.
+Sku: 10313354
+items: 1
+price: $7.97
+
+
+
+
+legenda frozen
+
+mulan
+hallowen
+minha mae é uma peça 3
+aves de rapina
+maria e joao conto das bruxas
+
+ 
+ */
